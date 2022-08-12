8dfa732db36a47af830db6fcc6fcff7de159a5d0
Shivani <shivani@ranium.in>
2022-08-12 11:02:58 +0530

    Initial Commit

---
+++ MyOmniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOmniApp
+//
+//  Created by Shivani Gupta on 12/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
